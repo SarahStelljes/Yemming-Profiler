2fc857de2788b33bf99fa2f6be2650da2b112466
Battlehart <sarah.stelljes@gmail.com>
2022-08-01 15:23:43 -0400

    added comments to index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const inquirer = require('inquirer');
-const process = require('node:process');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const generatePage = require('./src/page-template');
 const { writeFile, copyFile } = require('./utils/generate-site');
 
-const promptTeamManager = () => {
+// the first function to run when app starts. It creates the manager questions.
+const questions = () => {
+    // returns the answers to each question.
     return inquirer.prompt([
         {
             type: 'input',
@@ -63,10 +64,15 @@ const promptTeamManager = () => {
     ]);
 };
 
+// is the menu for either creating a new Engineer profile, Intern profile, and for finishing the team's profiles (meaning creates the html and copy css).
 const promptMenu = data => {
+    // if data employee doesn't exist
     if(!data.employee){
+        // create new manager by transfering answers from the data passed through
         const manager = new Manager(data.name, data.managerEmail, data.employeeID, data.officeNumber);
+        // resets the data object
         data = { };
+        // creates a new object named manager within a new object named employee within the data object.
         data.employee = {
             manager:{
                 role: manager.getRole(),
@@ -85,6 +91,7 @@ const promptMenu = data => {
     ===================
     `);
 
+    // returns the chosen option
     return inquirer.prompt([
         {
             type: 'list',
@@ -93,7 +100,11 @@ const promptMenu = data => {
             choices: ['Engineer', 'Intern', 'Finish Building Your Team']
         }
     ])
+    // passes through the chosen option
     .then(menu => {
+        // if the chosen option is a certain word/phrase, then...
+        // if there is no data.option yet, then create one and name it to the chosen option's word/phrase
+        // or if there is a data.option, then change it to the chosen option's word/phrase
         if(menu.menuOptions === 'Engineer'){
             if(!data.option){
                 data.option = 'Engineer';
@@ -113,11 +124,15 @@ const promptMenu = data => {
                 data.option = 'finish build';
             }
         }
+        // returns all the data, new and old
         return data;
     });
 };
 
+
+// function to make an Engineer's profile
 const makeEngineer = data => {
+    // if engineers does not not exist in data.employee yet, then make it so.
     if(!data.employee.engineers){
         data.employee.engineers = [];
     }
@@ -126,6 +141,7 @@ const makeEngineer = data => {
     Add an Engineer
     ===============
     `);
+    // returns all the inputed answers from the questions
     return inquirer.prompt([
         {
             type: 'input',
@@ -181,8 +197,10 @@ const makeEngineer = data => {
         }
     ])
     .then(engineerData => {
+        // creates new Engineer class by using the passed through data about the engineer
         const engineer = new Engineer(engineerData.engineerName, engineerData.engineerEmail, engineerData.engineerID, engineerData.engineerGitHub);
 
+        // pushes the data from the newly formed Engineer class into the engineers array
         data.employee.engineers.push({
             role: engineer.getRole(),
             name: engineer.getName(),
@@ -190,10 +208,12 @@ const makeEngineer = data => {
             id: engineer.getId(),
             roleSpecific: engineer.getGithub()
         });
+        // returns all the data, new and old.
         return data;
     })
 };
 
+// all is the same as makeEngineer function
 const makeIntern = data => {
     if(!data.employee.interns){
         data.employee.interns = [];
@@ -270,6 +290,8 @@ const makeIntern = data => {
         return data;
     })
 };
+
+// finish making the html by using the data passed through
 const finsihBuild = data => {
     const employee = data.employee;
     var employees = [];
@@ -285,38 +307,51 @@ const finsihBuild = data => {
             employees.push(employee.interns[i]);
         };
     }
+    // creates a new promise for the employees array and returns it.
     return Promise.resolve(employees);
 };
 
+// a function to be called over and over again until the user chooses the option "Finish building the team"
 const dataOptions = data => {
+    // calls the menu and passes the data through
     promptMenu(data)
     .then(data => {
+        // if the chosen option was Engineer, then call the makeEngineer function and pass the data through.
+        // once the newly formed data is added, then it takes you back to the menu.
         if(data.option === 'Engineer'){
             makeEngineer(data).then(data => {dataOptions(data)});
         } else if(data.option === 'Intern'){
+            // same as above, just for intern
             makeIntern(data).then(data => {dataOptions(data)});
         } else {
+            // if finish build was chosen, then...
             finsihBuild(data)
                 .then(employeeDataArr => {
+                    // passes the data through a module which returns the data as a string
                     const dataArr = generatePage(employeeDataArr);
                     return dataArr;
                 })
                 .then(pageHTML => {
+                    // creates index.html using the passed through data string and returns a response from the module
                     return writeFile(pageHTML);
                 })
                 .then(writeFileResponse => {
+                    // tells you the response from the first module
                     console.log(writeFileResponse.message);
+                    // copies the css file in src folder and puts the copied version into dist folder, then returns a message
                     return copyFile();
                 })
                 .then(copyFileResponse => {
+                    // tells you the message from the module's copyFile function.
                     console.log(copyFileResponse.message);
                 })
                 .catch(err => {
+                    // tells you any errors that occurs.
                     console.log(err);
                 });
         }
     });
 };
 
-promptTeamManager()
+questions()
     .then(dataOptions);
